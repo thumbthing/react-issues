aac8a9c66aa111e96bd970bd47477f64b0b630e8
thumbthing <ilscc0902@gmail.com>
2023-08-30 05:13:18 +0900

    Feat: Ad 클릭시 이동될 url 추가

--- src/components/Advertisements.tsx
+++ src/components/Advertisements.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ADVERTISEMENT_URL = 'https://www.wanted.co.kr/';
 
 function Advertisements() {
 	const imageURL = `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100`;
 
 	return (
 		<div>
-			<img alt="advertisement" src={imageURL} />
+			<Link to={ADVERTISEMENT_URL} target="_self">
+				<img alt="advertisement" src={imageURL} />
+			</Link>
 		</div>
 	);
 }
