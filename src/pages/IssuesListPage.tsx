b7f4c9f6afb5b0b265eb66a739990a2721fc43b8
thumbthing <ilscc0902@gmail.com>
2023-08-30 14:40:19 +0900

    Feat: Issues List 페이지 선언 (Issues 서버요청, 스크롤 이벤트로 데이터 요청)

--- src/pages/IssuesListPage.tsx
+++ src/pages/IssuesListPage.tsx
@@ -1,41 +1,85 @@
-import React, { useEffect, useState } from 'react';
-// import IssueItem from '../components/IssueItem';
+import React, { useEffect, useRef, useState } from 'react';
 import { getIssueList } from '../API/github.issue';
 import { IssueListType } from '../types/issue';
 import IssueItem from '../components/IssueItem';
 import Advertisements from '../components/Advertisements';
+import { activateInterceptor, deactivateInterceptor } from '../API/instance';
+import Loading from '../components/Loading';
 
 function IssuesListPage() {
 	const [issues, setIssues] = useState<IssueListType[]>([]);
+	const observerRef = useRef<IntersectionObserver>();
+	const containerRef = useRef<HTMLDivElement>(null);
+	const [isLoading, setIsLoading] = useState(false);
 
+	// interceptor 작동 후 state에 추가
+	const getIssuesAndAddToState = async () => {
+		try {
+			const interceptor = activateInterceptor(issues);
+			setIsLoading(true);
+			const response = await getIssueList();
+			const newIssues = [...issues].concat(response?.data);
+			setIssues(newIssues);
+			deactivateInterceptor(interceptor);
+		} catch (error) {
+			alert(error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	// 처음에 데이터 가져오는것
 	useEffect(() => {
-		const setIssueList = async () => {
-			try {
-				const response = await getIssueList();
-				if (response?.status === 200) {
-					setIssues(response?.data);
-				} else {
-					throw response?.status;
-				}
-			} catch (error) {
-				if (error instanceof Error) {
-					console.error('expected error occured : ', error);
-				} else {
-					console.error('unexpected error occured : ', error);
-				}
-			}
-		};
-
-		setIssueList();
+		getIssuesAndAddToState();
 	}, []);
 
+	// Intersection 설정
+	const intersectionObserverSetting = (entries: IntersectionObserverEntry[], io: IntersectionObserver) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				getIssuesAndAddToState();
+				io.unobserve(entry.target);
+			}
+		});
+	};
+
+	// IO 설정
 	useEffect(() => {
-		console.log(issues);
+		if (containerRef.current) {
+			observerRef.current = new IntersectionObserver(intersectionObserverSetting, {
+				root: null,
+				threshold: 0.5,
+			});
+			observerRef.current.observe(containerRef.current);
+		}
+
+		return () => {
+			if (observerRef.current) {
+				observerRef.current.disconnect();
+			}
+		};
+	}, []);
+
+	// 마지막 요소를 선택하여 Intersection Observer 적용
+	useEffect(() => {
+		if (containerRef.current) {
+			const lastElement = containerRef.current.querySelector('ul > li:last-child');
+			if (lastElement) {
+				setIsLoading(true);
+				const lastElementObserver = new IntersectionObserver(intersectionObserverSetting, {
+					root: null,
+					threshold: 0.5,
+				});
+				lastElementObserver.observe(lastElement);
+			}
+			setIsLoading(false);
+		}
 	}, [issues]);
 
 	return (
-		<div>
+		<div ref={containerRef}>
 			<ul>
+				{isLoading && <Loading />}
 				{issues.map((item, index) => (
 					<React.Fragment key={index}>
 						{index === 0
@@ -54,26 +98,5 @@ function IssuesListPage() {
 		</div>
 	);
 }
-
-// 해야할일 정리
-
-// header
-// 두 페이지는 공통 헤더를 공유
-// 헤더에는 Oragnization Name / Repository Name이 표시 되어야함
-
-// 이슈 목록 가져오기
-// open 상태인 이슈 중 코멘트가 많은 순 정렬
-// 각 행에는
-// 1. 이슈번호
-// 2. 이슈제목
-// 3. 작성자
-// 4. 작성장
-// 5. 코멘트수
-
-// 1-4 이슈
-// 5 광고 이미지
-//    광고 이미지에는 https://www.wanted.co.kr/ 로 이동
-
-// 화면 스크롤할시 이슈 목록 추가 로딩 (30개씩 받아진다)
 
 export default IssuesListPage;
