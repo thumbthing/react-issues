a359e1b5e1e2e904df08df910949a83aa17c1ae8
thumbthing <ilscc0902@gmail.com>
2023-08-30 14:33:59 +0900

    Feat: state에 따른 interceptor의 use, eject 함수 선언

--- src/API/instance.ts
+++ src/API/instance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { IssueListType } from '../types/issue';
 
 const instance = axios.create({
 	baseURL: process.env.REACT_APP_REPO,
@@ -10,11 +11,29 @@ const instance = axios.create({
 		state: 'open',
 		sort: 'comments',
 		direction: 'desc',
-		per_page: 10,
-		page: 1,
+		per_page: 5,
+		page: 0,
 	},
 });
 
-// TODO: interceptor 추가 필요
+// state: issue가 초기값인지 아닌지 판별 후 interceptor 적용
+export const activateInterceptor = (issues: IssueListType[]) => {
+	const pageValue = issues.length;
+	const interceptor = instance.interceptors.request.use((config) => {
+		if (pageValue / config.params.per_page >= 1) {
+			const newConfig = { ...config, params: { ...config.params, page: pageValue } };
+			return newConfig;
+		}
+		return config;
+	});
+	return interceptor;
+};
+
+// interceptor를 적용하고 state를 최신화 후 eject 함
+export const deactivateInterceptor = (interceptor: number | null) => {
+	if (interceptor !== null) {
+		instance.interceptors.request.eject(interceptor);
+	}
+};
 
 export default instance;
