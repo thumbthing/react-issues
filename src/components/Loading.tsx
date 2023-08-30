f48ff63b24459b774b852abf66a67c8f67559760
thumbthing <ilscc0902@gmail.com>
2023-08-30 14:36:54 +0900

    Feat: Loading 컴포넌트, 스타일링 선언

---
+++ src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoadingContainer from '../styles/Loading.style';
+
+function Loading() {
+	return (
+		<LoadingContainer>
+			<h1>Loading</h1>
+		</LoadingContainer>
+	);
+}
+
+export default Loading;
---
+++ src/styles/Loading.style.ts
@@ -0,0 +1,15 @@
+import { styled } from 'styled-components';
+
+const LoadingContainer = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(255, 255, 255, 0.7);
+`;
+
+export default LoadingContainer;
